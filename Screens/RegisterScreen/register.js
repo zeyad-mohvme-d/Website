25be9e98cefd6c4c6f0f98bbe582f13b522a9235
Zeyad Mohamed <zozmoha2005@gmail.com>
2025-04-24 12:14:06 +0200

    register.js file

---
+++ Screens/RegisterScreen/register.js
@@ -0,0 +1,14 @@
+function create() {
+    const username = document.getElementById('name').value;
+    const password = document.getElementById('password').value;
+
+    if (username && password) {
+        // Save the username and password to localStorage
+        localStorage.setItem('username', username);
+        localStorage.setItem('password', password);
+
+        alert("Registration successful! Welcome " + username);
+    } else {
+        alert("Please fill in both fields.");
+    }
+}
